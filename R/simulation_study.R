1fbc2cfb7bd079db1f80d9d525cd80ec1544b32f
mdanb <mdanb99@gmail.com>
2025-06-06 14:24:27 -0700

    finalize

--- R/simulation_study.R
+++ R/simulation_study.R
@@ -60,15 +60,3 @@ perform_simulation <- function(imp_numbers, trials, n_vec, alpha_vec) {
   return(n_level_list)
 }
 
-# n_level_list = perform_simulation(imp_numbers = 1:15,
-#                                   trials=3,
-#                                   n_vec=c(10,15,20),
-#                                   alpha_vec=c(0.5,0.75,0.95))
-# # it's always useful to save the date and R session info
-# date_of_run <- Sys.time()
-# session_info <- devtools::session_info()
-# 
-# save(n_level_list, # save your results
-#      alpha_vec, # save which alphas you used (for convenience)
-#      date_of_run, session_info,
-#      file = "~/demo_simulation.RData")
